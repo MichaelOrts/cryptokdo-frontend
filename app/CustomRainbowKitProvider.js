f8c00b2c97cef7e0902e7fa0b77886ab29c1425b
Michael Orts <michael.orts.86@gmail.com>
2024-06-30 01:01:36 +0200

    add footer and header with logo and web3 connect button

---
+++ app/CustomRainbowKitProvider.js
@@ -0,0 +1,35 @@
+'use client'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  hardhat
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'CryptoKDO',
+    projectId: '4b89f8ce0af198f46977dc74b4aaba58',
+    chains: [hardhat],
+    ssr: true, 
+});
+
+const queryClient = new QueryClient();
+const CustomRainbowKitProvider = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+                {children}
+            </RainbowKitProvider>
+        </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+export default CustomRainbowKitProvider
---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+      <footer className="bg-blue-600 text-white p-4 mt-auto">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2024 Web3 CryptoKDO App. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
